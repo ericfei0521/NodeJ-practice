8704871571afc9c1be75abe6b8134c3108201236
ericfei306 <eric0521bigjohn@gmail.com>
2023-11-07 23:11:24 +0800

    update git ignore

--- models/product.js
+++ models/product.js
@@ -1,14 +1,36 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+
+const productsPath = path.join(rootDir, 'data', 'products.json');
+
+const getProdctsFromFile = (cb) => {
+    fs.readFile(productsPath, (error, fileContent) => {
+        if (error) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
 
 module.exports = class Product {
-    constructor(title) {
+    constructor(title, imageUrl, description, price) {
         this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.price = price;
     }
-
     save() {
-        products.push(this);
+        getProdctsFromFile((products) => {
+            console.log('this', this);
+            products.push(this);
+            fs.writeFile(productsPath, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProdctsFromFile(cb);
     }
 };
