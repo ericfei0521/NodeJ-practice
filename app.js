d043310fd1be93528c78e24cd22c91fee685ab82
Eric Fei <eric.fei@partipost.com>
2023-11-07 21:15:34 +0800

    basic set up

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const errorControllers = require('./controllers/errors');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorControllers.get404page);
+
+app.listen(3000);
